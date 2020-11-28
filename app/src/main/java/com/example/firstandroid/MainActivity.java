5f00f02c14dff816f0c0759fd12f4538bdf69824
Andrey Dubinin <rakdav@inbox.ru>
2020-11-28 18:31:05 +0200

    1

--- app/src/main/java/com/example/firstandroid/MainActivity.java
+++ app/src/main/java/com/example/firstandroid/MainActivity.java
@@ -3,21 +3,50 @@ package com.example.firstandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText summaEditText;
+    private SeekBar seekBar;
+    private TextView textSumma;
+    private TextView textTips;
+    private TextView textTotal;
+    private Button send;
+    private TextView result;
+    private double summa,total,tips;
+    private int procenttips;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        LinearLayout linearLayout=new LinearLayout(this);
-//        linearLayout.setOrientation(LinearLayout.VERTICAL);
-//        TextView textView1=new TextView(this);
-//        textView1.setText("Hello");
-//        linearLayout.addView(textView1,new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT));
-//        setContentView(linearLayout);
+        summaEditText=findViewById(R.id.summa);
+        seekBar=findViewById(R.id.teeps);
+        textSumma=findViewById(R.id.textsumma);
+        textTips=findViewById(R.id.texttips);
+        textTotal=findViewById(R.id.texttotal);
+        send=findViewById(R.id.send);
+        result=findViewById(R.id.result);
+        summaEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 }
