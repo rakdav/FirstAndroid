ca541eaad53013b3f835d7598b63481a39ac8b9f
Andrey Dubinin <rakdav@inbox.ru>
2020-11-28 18:20:51 +0200

    Initial commit

---
+++ app/src/main/java/com/example/firstandroid/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.firstandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        LinearLayout linearLayout=new LinearLayout(this);
+//        linearLayout.setOrientation(LinearLayout.VERTICAL);
+//        TextView textView1=new TextView(this);
+//        textView1.setText("Hello");
+//        linearLayout.addView(textView1,new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT));
+//        setContentView(linearLayout);
+
+    }
+}
